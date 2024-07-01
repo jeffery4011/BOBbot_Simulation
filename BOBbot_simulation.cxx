38de553ac7b20f2994ffee92a8ebb95aa90fa20d
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-01 15:10:07 -0400

    Update BOBbot_simulation.cxx

--- BOBbot_simulation.cxx
+++ BOBbot_simulation.cxx
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
   if (argc <2){
     const int n = 10;
   }
-  const int n = std::stod(argv[1]);
+  // int n = std::stod(argv[1]);
   // BOBbot * pArray = new BOBbot[n];
   // for (int i =0;i<n;i++){
   //   pArray[i].disp_pos();
