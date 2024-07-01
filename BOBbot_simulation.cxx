5aad202853114652d7c0ec21a967a077d16287ac
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-01 15:32:05 -0400

    Update BOBbot_simulation.cxx

--- BOBbot_simulation.cxx
+++ BOBbot_simulation.cxx
@@ -6,18 +6,18 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc <2){
-    const double n = 10;
-  }
-  // const int n = 10;
+  // if (argc <2){
+  //   const double n = 10;
+  // }
+  // // const int n = 10;
   double n = std::stoi(argv[1]);
-  int index_max = (int) n;
-  BOBbot * pArray = new BOBbot[index_max];
-  for (int i =0;i<index_max;i++){
-    pArray[i].disp_pos();
-    pArray[i].disp_direction();
+  // int index_max = (int) n;
+  // BOBbot * pArray = new BOBbot[index_max];
+  // for (int i =0;i<index_max;i++){
+  //   pArray[i].disp_pos();
+  //   pArray[i].disp_direction();
 
-  }
+  // }
   
   
   return 0;
