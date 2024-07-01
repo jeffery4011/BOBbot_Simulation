742569da8b7cf22b0d87711dd9e3af9fad884e42
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-01 15:34:07 -0400

    Update BOBbot_simulation.cxx

--- BOBbot_simulation.cxx
+++ BOBbot_simulation.cxx
@@ -6,11 +6,23 @@
 
 int main(int argc, char* argv[])
 {
+    if (argc < 2) {
+    // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
+    //          and Tutorial_VERSION_MINOR
+    // std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+    //           << Tutorial_VERSION_MINOR << std::endl;
+    // std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    return 1;
+  }
+
+  // convert input to double
+  // TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
+  const double inputValue = std::stod(argv[1]);
   // if (argc <2){
   //   const double n = 10;
   // }
   // // const int n = 10;
-  double n = std::stoi(argv[1]);
+  // double n = std::stoi(argv[1]);
   // int index_max = (int) n;
   // BOBbot * pArray = new BOBbot[index_max];
   // for (int i =0;i<index_max;i++){
