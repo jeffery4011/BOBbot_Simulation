3374803f68a7de5c3eab9296693a896df842160b
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-02 11:49:38 -0400

    Update v2.h

--- BOBbot/v2.h
+++ BOBbot/v2.h
@@ -12,7 +12,7 @@ public:
 	v2(float xIn, float yIn);
 	void randomize();
 	void normalize();
-	void norm();
+	double norm();
 	v2 denominate(float d);
 	double dot(v2 vector);
 	v2 product(double f);
