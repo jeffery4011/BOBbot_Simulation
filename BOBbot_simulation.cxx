adcc353c603f2f2bd1f995ff0aa76cf9507244de
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-02 12:21:13 -0400

    Update BOBbot_simulation.cxx

--- BOBbot_simulation.cxx
+++ BOBbot_simulation.cxx
@@ -24,10 +24,18 @@ int main(int argc, char* argv[])
   // convert input to double
   // TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
   
-  BOBbot * pArray = new BOBbot[n];
+  BOBbot * BOBbotArray = new BOBbot[n];
   for (int i =0;i<n;i++){
-    pArray[i].disp_pos();
-    pArray[i].disp_External_Force();
+    BOBbotArray[i].disp_pos();
+    BOBbotArray[i].disp_External_Force();
+    v2 Force();
+    Force.normalize();
+    v2 Force_position();
+    Force_position.normalize();
+    Force_position.product(BOBbotArray[i].radius);
+    Force_position.sum(BOBbotArray[i].position);
+    BOBbotArray[i].apply_external_force(Force_position,Force_position);
+    BOBbotArray[i].disp_External_Force();
 
   }
   
