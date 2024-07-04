ac97ce47a5ba91baf4f251bb9396c7d5d0aea3e0
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-04 19:16:40 -0400

    Update interact_force.h

--- Interaction_Force/interact_force.h
+++ Interaction_Force/interact_force.h
@@ -1,3 +1,3 @@
 
-void Repel_Force((BOBbot BOBbotA, BOBbot BOBbotB,v2 distance, double h));
+void Repel_Force(BOBbot BOBbotA, BOBbot BOBbotB,v2 distance, double h);
 void External_Force_update(BOBbot *BOBbotArray, double h, int Num_of_BOBbot);
