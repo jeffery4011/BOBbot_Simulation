f03ba26131a75c599b30e7dc1f66a47cc58321df
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-04 16:27:47 -0400

    Update interact_force.cxx

--- Interaction_Force/interact_force.cxx
+++ Interaction_Force/interact_force.cxx
@@ -16,7 +16,8 @@ void External_Force_update(BOBbot *BOBbotArray, double h){
                 v2 collision_position = direction.product(0.5*distance.norm());
                 BOBbotArray[i].apply_external_force(collision_position,direction.product(-(k*repulsive_distance)));
                 BOBbotArray[j].apply_external_force(collision_position,direction.product(-(k*repulsive_distance)));
-                
+                BOBbotArray[i].disp_External_Force();
+                BOBbotArray[j].disp_External_Force();
             }
         }
     }
