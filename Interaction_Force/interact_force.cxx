8506e6807847f896dd849f660da6252917608fe8
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-04 16:29:46 -0400

    Update interact_force.cxx

--- Interaction_Force/interact_force.cxx
+++ Interaction_Force/interact_force.cxx
@@ -1,6 +1,6 @@
 #include "BOBbot.h"
 #include "v2.h"
-
+#include <iostream>
 #include <math.h>
 #include <cmath>
 
@@ -10,6 +10,7 @@ void External_Force_update(BOBbot *BOBbotArray, double h){
     for(int i =0;i<Num_of_BOBbot;i++){
         for(int j =i+1;j<Num_of_BOBbot;j++){
             v2 distance = BOBbotArray[i].position.minus(BOBbotArray[j].position);
+            std::cout<<"Distance  "<<distance.norm()<<std::endl;
             if (distance.norm()<(BOBbotArray[i].radius+BOBbotArray[j].radius+h)){
                 double repulsive_distance = BOBbotArray[i].radius+BOBbotArray[j].radius+h-distance.norm();
                 v2 direction = distance.product(1/distance.norm());
