7b9abac75733d2f6d45e782aa062a8172f0a95b2
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-06 10:19:23 -0400

    Update contact.cxx

--- Contact/contact.cxx
+++ Contact/contact.cxx
@@ -4,6 +4,6 @@
 #include <math.h>
 #include <cmath>
 
-bool contact(const Particle b1, const Particle b2){
+bool contact(Particle b1, Particle b2){
     return ((b1.position.minus(b2.position)).norm()<(b1.radius+b2.radius));
 }
