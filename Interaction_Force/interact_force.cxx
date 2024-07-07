ab36f70666c0f44eb2cd2bbe4df4f1b6d2662d11
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-07 16:37:54 -0400

    Update interact_force.cxx

--- Interaction_Force/interact_force.cxx
+++ Interaction_Force/interact_force.cxx
@@ -73,7 +73,7 @@ void External_Force_update(Particle *ParticleArray,const double h,const int Num_
                 v2 direction = distance.product(1/distance.norm());
                 v2 collision_position = ParticleArray[j].position.sum(direction.product(0.5*distance.norm()));
                 Damping_Shear_Force(ParticleArray[i],ParticleArray[j],collision_position);
-                Repulsive_Force(ParticleArray[i],ParticleArray[j],distance,direction,collision_position,h);
+                Repulsive_Force(ParticleArray[i],ParticleArray[j],distance,direction,collision_position);
                 ParticleArray[i].disp_External_Torque();
                 ParticleArray[i].disp_vel();
                 ParticleArray[i].disp_angular_vel();
