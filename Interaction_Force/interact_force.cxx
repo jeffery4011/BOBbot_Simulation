c02c6c327f523c133674a76337d699bb56abe1bd
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-06 22:15:09 -0400

    Update interact_force.cxx

--- Interaction_Force/interact_force.cxx
+++ Interaction_Force/interact_force.cxx
@@ -24,17 +24,17 @@ void Damping_Shear_Force(Particle& ParticleA, Particle& ParticleB,v2 collision_p
     v2 ParticleA_point_Velocity = ParticleA.velocity.sum(particleA_tang_velocity);
     v2 ParticleB_point_Velocity = ParticleB.velocity.sum(particleB_tang_velocity);
     v2 relative_velocity = ParticleA_point_Velocity.minus(ParticleB_point_Velocity);
-    ParticleA.apply_external_force(collision_position,relative_velocity.product(-1*eta));
-    ParticleB.apply_external_force(collision_position,relative_velocity.product(eta));
-
+    ParticleA.apply_external_force(collision_position,relative_velocity.product(1*eta));
+    ParticleB.apply_external_force(collision_position,relative_velocity.product(-1*eta));
+    ParticleA.disp_External_Force();
+    ParticleB.disp_External_Force();
 
     double kt = 0.2;
     v2 relative_tang_velocity = particleA_tang_velocity.minus(particleB_tang_velocity);
     // std::cout<<"Relative_tanget_velocity: "<< relative_tang_velocity.x<<"   "<<relative_tang_velocity.y<<std::endl;
     ParticleA.apply_external_force(collision_position,relative_tang_velocity.product(1*kt));
     ParticleB.apply_external_force(collision_position,relative_tang_velocity.product(-1*kt));
-    ParticleA.disp_External_Torque();
-    ParticleB.disp_External_Torque();
+    
 
 }
 
@@ -59,9 +59,9 @@ void External_Force_update(Particle *ParticleArray,const double h,const int Num_
             if (distance.norm()<(ParticleArray[i].radius+ParticleArray[j].radius+h)){
                 v2 direction = distance.product(1/distance.norm());
                 v2 collision_position = ParticleArray[j].position.sum(direction.product(0.5*distance.norm()));
-                Repulsive_Force(ParticleArray[i],ParticleArray[j],distance,direction,collision_position);
-                Damping_Shear_Force(ParticleArray[i],ParticleArray[j],collision_position);
                 
+                Damping_Shear_Force(ParticleArray[i],ParticleArray[j],collision_position);
+                Repulsive_Force(ParticleArray[i],ParticleArray[j],distance,direction,collision_position);
                 // ParticleArray[i].disp_External_Force();
                 // ParticleArray[j].disp_External_Force();
             }
