227f46f52138459e6b00f1ac87b6ca049aa7b73b
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-06 21:45:17 -0400

    Update interact_force.cxx

--- Interaction_Force/interact_force.cxx
+++ Interaction_Force/interact_force.cxx
@@ -30,9 +30,11 @@ void Damping_Shear_Force(Particle& ParticleA, Particle& ParticleB,v2 collision_p
 
     double kt = 2;
     v2 relative_tang_velocity = particleA_tang_velocity.minus(particleB_tang_velocity);
-    std::cout<<"Relative_tanget_velocity: "<< relative_tang_velocity.x<<"   "<<relative_tang_velocity.y<<std::endl;
+    // std::cout<<"Relative_tanget_velocity: "<< relative_tang_velocity.x<<"   "<<relative_tang_velocity.y<<std::endl;
     ParticleA.apply_external_force(collision_position,relative_tang_velocity.product(-1*kt));
     ParticleB.apply_external_force(collision_position,relative_tang_velocity.product(1*kt));
+    ParticleA.disp_External_Torque();
+    ParticleB.disp_External_Torque();
 
 }
 
