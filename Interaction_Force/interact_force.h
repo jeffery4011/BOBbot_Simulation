1c7d41287c719cbfd3cdbf74e47a0f690841ec0c
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-07 16:49:56 -0400

    debug

--- Interaction_Force/interact_force.h
+++ Interaction_Force/interact_force.h
@@ -1,4 +1,4 @@
 
-void Repulsive_Force(Particle ParticleA, Particle ParticleB,v2 distance,v2 direction,v2 collision_position);
+void Repulsive_Force(Particle ParticleA, Particle ParticleB,v2 distance,v2 direction,v2 collision_position,double h);
 void Damping_Shear_Force(Particle& ParticleA, Particle& ParticleB,v2 collision_position);
 void External_Force_update(Particle *ParticleArray, double h, int Num_of_Particle);
