3ac42cd6addeb23438a1226c6724f08df657b365
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-06 20:35:15 -0400

    Update solver.cxx

--- Solver/solver.cxx
+++ Solver/solver.cxx
@@ -10,7 +10,7 @@ void brute_particle_update_solver(Particle *ParticleArray,const double dt,const 
     for(int Particle_index=0;Particle_index<Num_of_Particle;Particle_index++){
         ParticleArray[Particle_index].position= ParticleArray[Particle_index].position.sum((ParticleArray[Particle_index].velocity.product(dt)).sum(ParticleArray[Particle_index].External_Force.product(0.5*dt*dt/ParticleArray[Particle_index].mass)));
         ParticleArray[Particle_index].angular_velocity=ParticleArray[Particle_index].angular_velocity+ParticleArray[Particle_index].External_Torque*dt/ParticleArray[Particle_index].inertia;
-        ParticleArray[Particle_index].direction = ParticleArray[Particle_index].direction.sum(ParticleArray[Particle_index].direction.angular_velocity_rotate(ParticleArray[Particle_index].angular_velocity.product(dt)));
+        ParticleArray[Particle_index].direction = ParticleArray[Particle_index].direction.sum(ParticleArray[Particle_index].direction.angular_velocity_rotate(dt*ParticleArray[Particle_index].angular_velocity));
         ParticleArray[Particle_index].disp_direction();
         ParticleArray[Particle_index].direction.normalize();
         ParticleArray[Particle_index].velocity = ParticleArray[Particle_index].velocity.sum(ParticleArray[Particle_index].External_Force.product( dt/ParticleArray[Particle_index].mass));
