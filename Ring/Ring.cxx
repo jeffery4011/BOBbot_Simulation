35778d43deeb134b0d48b7624f8ab4819e9523f5
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-09 17:27:52 -0400

    Update Ring.cxx

--- Ring/Ring.cxx
+++ Ring/Ring.cxx
@@ -33,14 +33,14 @@ void Ring::initialize(double ring_radius){
     RingParticleArray[0].head.x = ring_radius;
     RingParticleArray[0].tail.y = -1*RingParticleArray[0].radius;
     RingParticleArray[0].tail.x = ring_radius;
-    // for (int i =1;i++;i<n){
-    //     RingParticleArray[i].position.y = cos(2*3.14159265*i/n);
-    //     RingParticleArray[i].position.x = sin(2*3.14159265*i/n);
-    //     RingParticleArray[i].tail.x = RingParticleArray[i-1].head.x; 
-    //     RingParticleArray[i].tail.y = RingParticleArray[i-1].head.y;
-    //     // RingParticleArray[i].update_head(); 
+    for (int i =1;i++;i<n){
+        RingParticleArray[i].position.y = cos(2*3.14159265*i/n);
+        RingParticleArray[i].position.x = sin(2*3.14159265*i/n);
+        RingParticleArray[i].tail.x = RingParticleArray[i-1].head.x; 
+        RingParticleArray[i].tail.y = RingParticleArray[i-1].head.y;
+        // RingParticleArray[i].update_head(); 
 
-    // }
+    }
 }
 
 void Ring::disp_particle(){
