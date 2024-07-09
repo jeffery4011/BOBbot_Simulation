f1b2cad87b54fc92d68d9a944dd2060faf897e68
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-09 17:25:03 -0400

    Update Ring.cxx

--- Ring/Ring.cxx
+++ Ring/Ring.cxx
@@ -27,20 +27,20 @@ void Ring::initialize(double ring_radius){
     RingParticleArray = new Ring_particle[n];
     // std::cout<<"Ring_particle_head_x: "<<RingParticleArray[0].head.x<<std::endl;
     
-    RingParticleArray[0].position.y =0;
-    RingParticleArray[0].position.x =ring_radius;
-    RingParticleArray[0].head.y = RingParticleArray[0].radius;
-    RingParticleArray[0].head.x = ring_radius;
-    RingParticleArray[0].tail.y = -1*RingParticleArray[0].radius;
-    RingParticleArray[0].tail.x = ring_radius;
-    for (int i =1;i++;i<n){
-        RingParticleArray[i].position.y = cos(2*3.14159265*i/n);
-        RingParticleArray[i].position.x = sin(2*3.14159265*i/n);
-        RingParticleArray[i].tail.x = RingParticleArray[i-1].head.x; 
-        RingParticleArray[i].tail.y = RingParticleArray[i-1].head.y;
-        // RingParticleArray[i].update_head(); 
+    // RingParticleArray[0].position.y =0;
+    // RingParticleArray[0].position.x =ring_radius;
+    // RingParticleArray[0].head.y = RingParticleArray[0].radius;
+    // RingParticleArray[0].head.x = ring_radius;
+    // RingParticleArray[0].tail.y = -1*RingParticleArray[0].radius;
+    // RingParticleArray[0].tail.x = ring_radius;
+    // for (int i =1;i++;i<n){
+    //     RingParticleArray[i].position.y = cos(2*3.14159265*i/n);
+    //     RingParticleArray[i].position.x = sin(2*3.14159265*i/n);
+    //     RingParticleArray[i].tail.x = RingParticleArray[i-1].head.x; 
+    //     RingParticleArray[i].tail.y = RingParticleArray[i-1].head.y;
+    //     // RingParticleArray[i].update_head(); 
 
-    }
+    // }
 }
 
 void Ring::disp_particle(){
