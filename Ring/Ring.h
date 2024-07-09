e1a1c8e5251dcaa4b009c0ddcb7bee1215cac60e
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-09 15:40:01 -0400

    Update Ring.h

--- Ring/Ring.h
+++ Ring/Ring.h
@@ -3,9 +3,9 @@
 #include "Particle.h"
 
 class Ring_particle: public Particle{
-    mass = 0.0000001;
-    radius = 0.0001;
-    intertia = 0.5*mass*radius*radius;
+    double mass = 0.0000001;
+    double radius = 0.0001;
+    double intertia = 0.5*mass*radius*radius;
 }
 
 class Ring {
@@ -13,5 +13,5 @@ class Ring {
     int n = 1000;
     Particle * RingParticleArray = new Ring_particle[n];
 
-    void initialize()
+    void initialize();
 };
