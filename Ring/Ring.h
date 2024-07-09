bc56fa8dd57d6bbb6f09bafd0b67eecf0f6af032
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-09 16:23:20 -0400

    Update Ring.h

--- Ring/Ring.h
+++ Ring/Ring.h
@@ -4,9 +4,7 @@
 
 class Ring_particle: public Particle{
     public:
-    double mass = 0.0000001;
-    double radius = 0.0001;
-    double intertia = 0.5*mass*radius*radius;
+    Ring_particle(){mass = 0.0000001;radius = 0.0001;intertia = 0.5*mass*radius*radius;}
 };
 
 class Ring {
