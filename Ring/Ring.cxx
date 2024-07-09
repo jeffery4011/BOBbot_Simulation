c5b2c7bc0ec68790f8f2a292d4fb26c81dacd24c
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-09 16:06:32 -0400

    Update Ring.cxx

--- Ring/Ring.cxx
+++ Ring/Ring.cxx
@@ -11,12 +11,12 @@
 //         RingParticleArray[i].disp_pos();
 //     }
 // }
-Ring::initialize(double ring_radius){
+void Ring::initialize(double ring_radius){
     n = 2*3.1415926*ring_radius/2/Ring_particle[0].radius;
     RingParticleArray = new Ring_particle[n];
 }
 
-Ring::disp_particle(){
+void Ring::disp_particle(){
     for (int i =0;i<n;i++){
         Ring.RingParticleArray[i].disp_pos();
     }
