29b557f8c6504c2a8e0a79a881493314c9e40540
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-11 18:32:01 -0400

    Update Ring.cxx

--- Ring/Ring.cxx
+++ Ring/Ring.cxx
@@ -55,7 +55,7 @@ void Ring::initialize(double ring_radius){
 void Ring::disp_particle(){
     for (int i =0;i<n;i++){
         RingParticleArray[i].disp_pos();
-        RingParticleArray[i].disp_External_Force();
+        //RingParticleArray[i].disp_External_Force();
     }
 }
 
@@ -69,6 +69,7 @@ void Ring::update_internal_force(){
         if (dis.norm() > 2*RingParticleArray[i].radius){
             v2 F = dis.product(kr*(2*RingParticleArray[i].radius - dis.norm()));
             RingParticleArray[i-1].apply_external_force(RingParticleArray[i-1].position,F);
+            RingParticleArray[i-1].disp_External_Force();
             RingParticleArray[i].apply_external_force(RingParticleArray[i].position,F.product(-1));
         }
 
