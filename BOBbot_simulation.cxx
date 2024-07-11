9376104f75ae73eda7dcea45b02bf6159402846d
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-11 11:40:02 -0400

    vector_particle_array

--- BOBbot_simulation.cxx
+++ BOBbot_simulation.cxx
@@ -32,8 +32,39 @@ int main(int argc, char* argv[])
   int max_num_of_try = 10000;
   int place_index = 1;
   int num_of_try = 0;
+  // while (place_index <n){
+  //   std::cout<<"Placing "<<place_index<<"th Particle"<<std::endl;
+  //   num_of_try = 0;
+  //   bool place_success = 0;
+  //   while (! place_success){
+  //     std::cout<<"Try "<<num_of_try<<"th time"<<std::endl;
+  //     int contact_index = 0;
+  //       while (contact_index<place_index){
+        
+  //         if (contact(ParticleArray[contact_index],ParticleArray[place_index])){
+  //           num_of_try++;
+  //           ParticleArray[place_index] = Particle();
+  //           break;
+  //         }
+  //         contact_index++;
+  //       }
+        
+  //       if (contact_index == place_index){
+  //         place_success =1;
+  //       }
+  //       if (num_of_try == max_num_of_try){
+  //         std::cout<<"Over Crowded! Decrease the number of robot or vary the distribution parameter!"<<std::endl;
+  //         return 0;
+  //       }
+  //   }
+  //   place_index++;
+  // }std::cout<<"All Robots have been placed!"<<std::endl;
+
+
+  vector_particle_array.push_back(std::make_shared<Particle>());
   while (place_index <n){
     std::cout<<"Placing "<<place_index<<"th Particle"<<std::endl;
+    vector_particle_array.push_back(std::make_shared<Particle>());
     num_of_try = 0;
     bool place_success = 0;
     while (! place_success){
@@ -41,9 +72,9 @@ int main(int argc, char* argv[])
       int contact_index = 0;
         while (contact_index<place_index){
         
-          if (contact(ParticleArray[contact_index],ParticleArray[place_index])){
+          if (contact(vector_particle_array[contact_index],vector_particle_array[place_index])){
             num_of_try++;
-            ParticleArray[place_index] = Particle();
+            vector_particle_array[0] = std::make_shared<Particle>();
             break;
           }
           contact_index++;
@@ -57,9 +88,7 @@ int main(int argc, char* argv[])
           return 0;
         }
     }
-    place_index++;
-  }std::cout<<"All Robots have been placed!"<<std::endl;
-
+  }
   for(int i=0;i<n;i++){
     vector_particle_array.push_back(std::make_shared<Particle>());
     std::cout<<vector_particle_array[i]->position.x<<std::endl;
@@ -69,32 +98,32 @@ int main(int argc, char* argv[])
 
   //End of Check Overlapping
 
-  std::ofstream myfile;
-  Ring ring;
-  ring.initialize(0.2);
-  // ring.disp_particle();
-  std::cout<<"Robot_radius: "<<ParticleArray[0].radius<<std::endl;
-  std::cout<<"Ring_particle_num:_"<<ring.n<<std::endl;
-  myfile.open("Ring_num.txt", std::ios_base::app);
-    // myfile<<std::setprecision(4)<<std::fixed;
-  myfile<<ring.n<<std::endl;
-  myfile.close();
+  // std::ofstream myfile;
+  // Ring ring;
+  // ring.initialize(0.2);
+  // // ring.disp_particle();
+  // std::cout<<"Robot_radius: "<<ParticleArray[0].radius<<std::endl;
+  // std::cout<<"Ring_particle_num:_"<<ring.n<<std::endl;
+  // myfile.open("Ring_num.txt", std::ios_base::app);
+  //   // myfile<<std::setprecision(4)<<std::fixed;
+  // myfile<<ring.n<<std::endl;
+  // myfile.close();
   
 
-  Particle * TotalParticleArray = new Particle[n+ring.n];
-  std::vector<std::shared_ptr<Particle>> SumParticleArray;
-  std::copy(ParticleArray,ParticleArray+n,TotalParticleArray);
-  std::copy(ring.RingParticleArray,ring.RingParticleArray+ring.n,TotalParticleArray+n);
-  for (int i =0;i<ring.n;i++){
-    ring.RingParticleArray[i].position.x = ring.RingParticleArray[i].position.x +1;
-  }
+  // Particle * TotalParticleArray = new Particle[n+ring.n];
+  // std::vector<std::shared_ptr<Particle>> SumParticleArray;
+  // std::copy(ParticleArray,ParticleArray+n,TotalParticleArray);
+  // std::copy(ring.RingParticleArray,ring.RingParticleArray+ring.n,TotalParticleArray+n);
+  // for (int i =0;i<ring.n;i++){
+  //   ring.RingParticleArray[i].position.x = ring.RingParticleArray[i].position.x +1;
+  // }
 
-  for (int i =0;i<n+ring.n;i++){
-    myfile.open("Ring.txt", std::ios_base::app);
-    // myfile<<std::setprecision(4)<<std::fixed;
-    myfile<<TotalParticleArray[i].position.x<<" "<<TotalParticleArray[i].position.y<<std::endl;
-    myfile.close();
-  }
+  // for (int i =0;i<n+ring.n;i++){
+  //   myfile.open("Ring.txt", std::ios_base::app);
+  //   // myfile<<std::setprecision(4)<<std::fixed;
+  //   myfile<<TotalParticleArray[i].position.x<<" "<<TotalParticleArray[i].position.y<<std::endl;
+  //   myfile.close();
+  // }
 
 
   // savedata(ParticleArray);
--- Contact/contact.cxx
+++ Contact/contact.cxx
@@ -4,6 +4,6 @@
 #include <math.h>
 #include <cmath>
 
-bool contact(Particle b1, Particle b2){
-    return ((b1.position.minus(b2.position)).norm()<(b1.radius+b2.radius));
+bool contact(std::shared_ptr<Particle> b1, std::shared_ptr<Particle> b2){
+    return ((b1->position.minus(b2->position)).norm()<(b1->radius+b2->radius));
 }
