2ade45f5466ba3b9bdea1b74c0198cf54966310a
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-23 16:38:02 -0400

    spring force debug

--- Ring/Ring.cxx
+++ Ring/Ring.cxx
@@ -96,5 +96,10 @@ void Ring::update_internal_force(){
         RingParticleArray[0].External_Force = RingParticleArray[0].External_Force.sum(F.product(-1));
     }
     std::cout<<"Spring_Force"<<std::endl;
+    std::cout<<"Particle0:"<<std::endl;
+    RingParticleArray[0].disp_pos();
+    RingParticleArray[0].disp_External_Force();
+    std::cout<<"Particle1:"<<std::endl;
+    RingParticleArray[1].disp_pos();
     RingParticleArray[1].disp_External_Force();
 }
