51433ed14d438a58fce682fcbb799cea986d78d4
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-23 16:11:27 -0400

    collision_debug

--- BOBbot_simulation.cxx
+++ BOBbot_simulation.cxx
@@ -209,9 +209,9 @@ int main(int argc, char* argv[])
     myfileY<<std::endl;
     myfileY.close();
     
-    ring.RingParticleArray[0].disp_pos();
-    ring.RingParticleArray[0].disp_vel();
-    ring.RingParticleArray[0].disp_External_Force();
+    // ring.RingParticleArray[0].disp_pos();
+    // ring.RingParticleArray[0].disp_vel();
+    // ring.RingParticleArray[0].disp_External_Force();
   }
   // brute_particle_update_solver(ring.RingParticleArray,0.001,0,ring.n);
   // ring.disp_particle();
