f06c7628d930dde77312f7b251d75eb39c72a28c
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-25 18:28:15 -0400

    Update interact_force.cxx

--- Interaction_Force/interact_force.cxx
+++ Interaction_Force/interact_force.cxx
@@ -98,6 +98,8 @@ void External_Force_update(Particle *ParticleArray,const double h,const int Num_
                 std::cout<<"Collide between: "<<i<<"  "<<j<<std::endl;
                 v2 direction = distance.product(1/distance.norm());
                 v2 collision_position = ParticleArray[j].position.sum(direction.product(0.5*distance.norm()));
+                std::cout<<"String Force:";
+                ParticleArray[i].disp_External_Force();
                 Damping_Shear_Force(ParticleArray[i],ParticleArray[j],collision_position);
                 Repulsive_Force(ParticleArray[i],ParticleArray[j],distance,direction,collision_position,h);
                 // ParticleArray[i].disp_External_Torque();
